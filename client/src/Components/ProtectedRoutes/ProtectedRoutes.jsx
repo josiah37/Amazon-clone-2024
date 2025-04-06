8e1b61c0e2132eb41d6d1dda5c7f5f3604c97b2a
josiah37 <eyosi50@gmail.com>
2025-04-06 23:52:16 +0300

    the user was lost and was rediredicting to auth page upon refresh previously now i have set a loder in provider and the user is not lost upon refresh, another method you can use is to set the user in local storage but we are not using that currently

--- client/src/Components/ProtectedRoutes/ProtectedRoutes.jsx
+++ client/src/Components/ProtectedRoutes/ProtectedRoutes.jsx
@@ -1,23 +1,36 @@
 import { useContext, useEffect } from "react";
 import { DataContext } from "../../utils/DataProvider";
 import { redirect, useNavigate } from "react-router";
-
+import { FadeLoader } from "react-spinners";
+import styles from "../../Pages/Orders/Orders.module.css";
 // TODO: this is not  the standard way of doing it so replace this with the standard route protection
 export const ProtectedRoutes = ({ children, msg, redirect }) => {
    // destructuring the user directly
    const {
       state: { user },
-      dispatch,
+      loading,
    } = useContext(DataContext);
 
    const navigate = useNavigate();
    // for every user if the user does not exist, we redirect then to the auth and pass a msg and the to
    // the last page they were trying to visit before auth
    useEffect(() => {
-      if (!user) {
+      if (!loading && !user) {
          navigate("/auth", { state: { msg, redirect } });
       }
-   }, [user]);
+   }, [user, loading, navigate, msg, redirect]);
+
+   // if (loading) {
+   //    return <div>Loading...!!!</div>; // Render a loading state if user data is being fetched
+   // }
+
+   if (loading) {
+      return (
+         <div className={styles.ordersListloaderContainer}>
+            <FadeLoader color="#FFA500" loading={loading} />
+         </div>
+      );
+   }
 
    // return children;
    // the above return the children regadless of the user status so the user may see glimps of what the page holds
--- client/src/utils/DataProvider.jsx
+++ client/src/utils/DataProvider.jsx
@@ -1,12 +1,40 @@
-// src/Components/DataProvider/DataProvider.jsx
+// // src/Components/DataProvider/DataProvider.jsx
 
-import React, { createContext, useReducer } from "react";
+// import React, { createContext, useReducer } from "react";
+// import { reducer, initialState } from "../utils/reducer";
+
+// export const DataContext = createContext();
+
+// export const DataProvider = ({ children }) => {
+//    const [state, dispatch] = useReducer(reducer, initialState);
+
+//    return <DataContext.Provider value={{ state, dispatch }}>{children}</DataContext.Provider>;
+// };
+
+import React, { createContext, useReducer, useEffect, useState } from "react";
 import { reducer, initialState } from "../utils/reducer";
+import { getAuth, onAuthStateChanged } from "firebase/auth"; // Correct import
+import { Type } from "../utils/Action.type";
 
 export const DataContext = createContext();
 
 export const DataProvider = ({ children }) => {
    const [state, dispatch] = useReducer(reducer, initialState);
+   const [loading, setLoading] = useState(true);
+   const auth = getAuth(); // Get the Firebase auth instance
 
-   return <DataContext.Provider value={{ state, dispatch }}>{children}</DataContext.Provider>;
+   useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
+         if (user) {
+            dispatch({ type: Type.SET_USER, user }); // Update user if exists
+         } else {
+            dispatch({ type: Type.SET_USER, user: null }); // Set user state to null
+         }
+         setLoading(false); // Set loading to false
+      });
+
+      return () => unsubscribe(); // Clean up subscription
+   }, [auth, dispatch]);
+
+   return <DataContext.Provider value={{ state, dispatch, loading }}>{children}</DataContext.Provider>;
 };
